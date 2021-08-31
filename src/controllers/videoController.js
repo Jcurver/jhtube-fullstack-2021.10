9288448773859ecbe628f433753388b6af958411
ifizzyou <pjh15237213@gmail.com>
2021-08-31 09:31:54 +0900

    8.31

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,13 +1,9 @@
-export const trending = (req, res) => res.send("Trending");
+export const trending = (req, res) => res.render("home");
 
-export const see = (req, res) => {
-    console.log(req.params);
-    res.send(`Watch Video #${req.params.id}`);
-}
-export const edit = (req, res) => {
-    console.log(req.params);
-    res.send("Edit");
-}
+export const see = (req, res) => res.render("watch");
+
+export const edit = (req, res) => res.render("edit")
+
 
 export const search = (req, res) => res.send("Search");
 
@@ -17,3 +13,6 @@ export const deleteVideo = (req, res) => {
     console.log(req.params);
     res.send("Delete Video");
 }
+
+
+
