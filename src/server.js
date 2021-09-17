4643b572f73b22011fc8bcdf65ee35b5008a72a4
ifizzyou <pjh15237213@gmail.com>
2021-09-17 18:41:43 +0900

    9.17

--- src/server.js
+++ src/server.js
@@ -15,7 +15,9 @@ app.use(logger);
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
+
 const handleListening = () =>
   console.log(`😎Server listening on port http://localhost:${PORT}}`);
 
 app.listen(PORT, handleListening);
+
